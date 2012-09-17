8fd90c57ce19d771180aa8a136243d72335b6c71
Nachiketas Ramanujam <nketas@gmail.com>
2012-09-17 00:26:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ignite::Application.config.secret_token = '2c57fb27a67243a0df84d8b8615ed99df31cc0488914b1ae5a385732f36ba5f4115bbe50739ed3133a7e4c382bab7af5f3b7803d31a45e7e08c853128604e7a8'
